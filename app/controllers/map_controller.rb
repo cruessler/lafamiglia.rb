d51af1dcd5c6c536cdeec525b75813344197444c
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-01-11 23:08:13 +0100

    Add labels to map axes

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -18,15 +18,15 @@ class MapController < ApplicationController
       y = LaFamiglia.max_y
     end
 
-    min_x, max_x = x - MAP_RADIUS, x + MAP_RADIUS
-    min_y, max_y = y - MAP_RADIUS, y + MAP_RADIUS
+    @min_x, @max_x = x - MAP_RADIUS, x + MAP_RADIUS
+    @min_y, @max_y = y - MAP_RADIUS, y + MAP_RADIUS
 
-    villas = Villa.where([ 'x BETWEEN ? AND ? AND y BETWEEN ? AND ?', min_x, max_x, min_y, max_y ]).load
+    villas = Villa.where([ 'x BETWEEN ? AND ? AND y BETWEEN ? AND ?', @min_x, @max_x, @min_y, @max_y ]).load
 
-    @villas = Array.new(max_y - min_y + 1) { Array.new(max_x - min_x + 1) }
+    @villas = Array.new(@max_y - @min_y + 1) { Array.new(@max_x - @min_x + 1) }
 
     villas.each do |v|
-      @villas[v.y - min_y][v.x - min_x] = v
+      @villas[v.y - @min_y][v.x - @min_x] = v
     end
   end
 end
