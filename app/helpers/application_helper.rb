1aac009cbb5060d7bd4e6a316102367cf10cc7b2
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-01-20 23:55:20 +0100

    Replace villa_to_s by Villa#to_s

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,6 @@ module ApplicationHelper
            locals: { alert_type: alert_type, message: message }
   end
 
-  def villa_to_s villa
-    "#{villa.name} (#{villa.x}|#{villa.y})"
-  end
-
   def time_diff diff
     '%02d:%02d:%02d' % [ diff / 3600, (diff % 3600) / 60, diff % 60 ]
   end
