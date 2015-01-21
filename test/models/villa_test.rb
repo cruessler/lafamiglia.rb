b1d46029d1f6c5192077e95fec784b891a6fcdd9
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-01-21 21:19:39 +0100

    Add the first test

--- test/models/villa_test.rb
+++ test/models/villa_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class VillaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should update counter caches" do
+    v = villas(:one)
+    v.building_queue_items.enqueue LaFamiglia.building(1)
+    v.save
+
+    assert_equal 1, v.building_queue_items_count
+
+    v.building_queue_items.dequeue v.building_queue_items.last
+
+    assert_equal 0, v.building_queue_items_count
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,7 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  setup do
+    LaFamiglia.clock
+  end
 end
