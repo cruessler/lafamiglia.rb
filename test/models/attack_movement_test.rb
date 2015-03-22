d882c65bb5b82e8b36b5c1b6f23b81548c4a7234
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-03-22 22:00:42 +0100

    Add test

--- test/models/attack_movement_test.rb
+++ test/models/attack_movement_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class AttackMovementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @origin = villas(:one)
+    @target = villas(:two)
+
+    @origin.processed_until = LaFamiglia.now
+    @target.processed_until = LaFamiglia.now
+  end
+
+  test "way there and way back should have the same duration" do
+    @attack = AttackMovement.create(origin: @origin,
+                                    target: @target,
+                                    unit_1: 1)
+
+    LaFamiglia.clock(LaFamiglia.now + @attack.duration)
+
+    @comeback = @attack.cancel!
+
+    assert_equal (LaFamiglia.now + @attack.duration), @comeback.arrives_at
+  end
 end
