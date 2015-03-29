95829b4fac6393a3cc57b6d2b5d15cfb3add2919
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-03-29 18:00:43 +0200

    Add test

--- test/models/movement_test.rb
+++ test/models/movement_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MovementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @m = Movement.create(origin: villas(:one),
+                        target: villas(:two),
+                        unit_1: 1)
+  end
+
+  test "movement should have positive speed" do
+    assert @m.speed > 0
+  end
+
+  test "movement should have a positive duration" do
+    assert @m.duration > 0
+  end
 end
