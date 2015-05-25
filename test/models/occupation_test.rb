117ace525148cb466703acfb41bdc586e33b05b3
Christoph Rüßler <christoph.ruessler@mailbox.org>
2015-05-25 22:24:19 +0200

    Refactor OccupationTest

--- test/models/occupation_test.rb
+++ test/models/occupation_test.rb
@@ -2,29 +2,28 @@ require 'test_helper'
 
 class OccupationTest < ActiveSupport::TestCase
   setup do
-    @v1 = villas :one
-    @v2 = villas :two
+    setup_for_occupation_test
   end
 
   test "a villa should not be not occupied by more than one occupation" do
-    refute_predicate @v1, :occupied?
+    refute_predicate @origin, :occupied?
 
     occupation_1 = Occupation.create(succeeds_at: LaFamiglia.now + 1.hour,
                                      unit_1: 1,
-                                     origin: @v2,
-                                     target: @v1)
+                                     origin: @target,
+                                     target: @origin)
     assert_not_nil occupation_1
-    assert_predicate @v1, :occupied?
+    assert_predicate @origin, :occupied?
 
     assert_raises ActiveRecord::RecordNotUnique do
       occupation_2 = Occupation.create(succeeds_at: LaFamiglia.now + 1.hour,
                                        unit_1: 1,
-                                       origin: @v2,
-                                       target: @v1)
+                                       origin: @target,
+                                       target: @origin)
     end
 
     occupation_1.destroy
 
-    refute_predicate @v1, :occupied?
+    refute_predicate @origin, :occupied?
   end
 end
