83222a8a6dffdd54e9c005ecaa47422a15566480
Christoph Rüßler <christoph.ruessler@mailbox.org>
2018-07-01 16:19:02 +0200

    Use webpack instead of sprockets pipeline

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,30 +1,6 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery/dist/jquery
-//= require jquery-ujs/src/rails
-//= require bootstrap-sass/assets/javascripts/bootstrap/affix
-//= require bootstrap-sass/assets/javascripts/bootstrap/alert
-//= require bootstrap-sass/assets/javascripts/bootstrap/button
-//= require bootstrap-sass/assets/javascripts/bootstrap/carousel
-//= require bootstrap-sass/assets/javascripts/bootstrap/collapse
-//= require bootstrap-sass/assets/javascripts/bootstrap/dropdown
-//= require bootstrap-sass/assets/javascripts/bootstrap/tab
-//= require bootstrap-sass/assets/javascripts/bootstrap/transition
-//= require bootstrap-sass/assets/javascripts/bootstrap/scrollspy
-//= require bootstrap-sass/assets/javascripts/bootstrap/modal
-//= require bootstrap-sass/assets/javascripts/bootstrap/tooltip
-//= require bootstrap-sass/assets/javascripts/bootstrap/popover
-//= require handlebars.runtime
-//= require typeahead.js/dist/typeahead.bundle
-//= require globals
-//= require_tree .
+import css from '../stylesheets/application.scss'
+
+import 'jquery-ujs'
+import '../../../node_modules/bootstrap-sass/assets/javascripts/bootstrap'
+import 'typeahead.js'
+import 'handlebars/runtime'
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => ({
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: './app/assets/javascripts/application.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'public/assets/')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'resolve-url-loader',
+          { loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        use: [
+          { loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'app.css' }),
+    new CopyWebpackPlugin([{ from: 'app/assets/images', to: '' }]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery'
+    })
+  ]
+});
