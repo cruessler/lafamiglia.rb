3cff28123c5f4c62097d0da0fdbe4ea963777116
Christoph Rüßler <christoph.ruessler@mailbox.org>
2020-02-11 18:47:41 +0100

    Convert PlayerSelector to function component

--- app/assets/javascripts/selectors/player_selector.jsx
+++ app/assets/javascripts/selectors/player_selector.jsx
@@ -1,91 +1,125 @@
 // https://github.com/pqx/bloodhound
-import Bloodhound from "bloodhound-js";
-import React, { Component} from "react";
+import Bloodhound from 'bloodhound-js';
+import React, {
+  Component,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 // https://github.com/twitter/typeahead.js
-import Typeahead from "typeahead.js";
+import Typeahead from 'typeahead.js';
 
-class PlayerSelector extends Component {
-  constructor(props) {
-    super(props);
+const PlayerSelector = props => {
+  const bloodhound = useMemo(
+    () =>
+      new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        limit: 10,
+        identify: p => p.id,
+        remote: {
+          url: 'players/search/%QUERY',
+          wildcard: '%QUERY',
+        },
+      }),
+    []
+  );
 
-    this.bloodhound = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.whitespace,
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      limit: 10,
-      identify: p => p.id,
-      remote: {
-        url: "players/search/%QUERY",
-        wildcard: "%QUERY"
-      }
-    });
-
-    this.datasets = {
-      name: "players",
-      display: "name",
+  const datasets = useMemo(
+    () => ({
+      name: 'players',
+      display: 'name',
       templates: {
-        header: "<h4>Players</h4>",
-        suggestion: p => `<a href="#">${p.name}</a>`
+        header: '<h4>Players</h4>',
+        suggestion: p => `<a href="#">${p.name}</a>`,
       },
-      source: this.bloodhound.ttAdapter()
-    }
+      source: bloodhound.ttAdapter(),
+    }),
+    [bloodhound]
+  );
 
-    // This is used for initialization of typeahead in componentDidMount.
-    this.inputRef = React.createRef();
+  const inputRef = useRef();
 
-    this.state = { players: props.players };
+  const [players, setPlayers] = useState(props.players);
 
-    this.onClick = this.onClick.bind(this);
-  }
+  const addPlayer = useCallback(
+    (event, player) => {
+      if (!players.some(p => p.id == player.id)) {
+        const newPlayers = [...players, player];
 
-  componentDidMount() {
-    var typeaheadElement = this.inputRef.current;
-
-    $(typeaheadElement).typeahead({ minLength: 0 }, this.datasets);
-
-    $(typeaheadElement).bind("typeahead:select", (event, player) => {
-      if (!this.state.players.some(p => p.id == player.id)) {
-        this.state.players.push(player);
-
-        this.setState({ players: this.state.players });
+        setPlayers(newPlayers);
       }
-    });
-  }
+    },
+    [players]
+  );
 
-  onClick(id) {
-    this.setState({ players: this.state.players.filter((p) => p.id != id) });
-  }
+  const onClick = useCallback(
+    id => {
+      setPlayers(players.filter(p => p.id != id));
+    },
+    [players]
+  );
 
-  item(player) {
-    return(
-      <div key={player.id} className="btn-group player-selected">
-        <button className="btn btn-default btn-sm dropdown-toggle" type="button" data-toggle="dropdown">
-          {player.name} <span className="caret"></span>
-        </button>
-        <ul className="dropdown-menu" role="menu">
-          <li><a href="#" className="remove-player" onClick={(e) => this.onClick(player.id, e)}>Remove</a></li>
-        </ul>
+  useEffect(() => {
+    $(inputRef.current).typeahead({ minLength: 0 }, datasets);
+  }, []);
+
+  useEffect(
+    () => {
+      $(inputRef.current).bind('typeahead:select', addPlayer);
+    },
+    [addPlayer]
+  );
+
+  const item = player => (
+    <div key={player.id} className="btn-group player-selected">
+      <button
+        className="btn btn-default btn-sm dropdown-toggle"
+        type="button"
+        data-toggle="dropdown"
+      >
+        {player.name} <span className="caret" />
+      </button>
+      <ul className="dropdown-menu" role="menu">
+        <li>
+          <a
+            href="#"
+            className="remove-player"
+            onClick={() => onClick(player.id)}
+          >
+            Remove
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+
+  const hiddenItem = player => (
+    <input
+      key={player.id}
+      type="hidden"
+      name={`${props.name}[]`}
+      value={player.id}
+    />
+  );
+
+  return (
+    <div>
+      <div className="has-feedback">
+        <input
+          ref={inputRef}
+          type="text"
+          className="form-control player-search"
+          placeholder="Type to search …"
+        />
+        <span className="glyphicon glyphicon-search form-control-feedback" />
+        {players.map(item)}
+        {players.map(hiddenItem)}
       </div>
-    );
-  }
-
-  hiddenItem(player) {
-    return(
-      <input key={player.id} type="hidden" name={`${this.props.name}[]`} value={player.id}/>
-    );
-  }
-
-  render() {
-    return(
-      <div>
-        <div className="has-feedback">
-          <input ref={this.inputRef} type="text" className="form-control player-search" placeholder="Type to search …" />
-          <span className="glyphicon glyphicon-search form-control-feedback"></span>
-          {this.state.players.map((p) => this.item(p))}
-          {this.state.players.map((p) => this.hiddenItem(p))}
-        </div>
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default PlayerSelector;
