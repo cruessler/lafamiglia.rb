abc1119e07174db856c0cd9787b75506aed10ee7
Christoph Rüßler <christoph.ruessler@mailbox.org>
2020-05-24 12:39:57 +0200

    Remove gems related to Rails’ asset pipeline
    Front-end assets are managed by webpack

--- Gemfile
+++ Gemfile
@@ -10,15 +10,6 @@ gem 'rails-controller-testing'
 # Use PostgreSQL as the database for Active Record
 gem 'pg', '~> 1.2'
 
-# Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
-
-# Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
-
-# Use CoffeeScript for .js.coffee assets and views
-gem 'coffee-rails', '~> 5.0'
-
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,13 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile << /\.(?:png|gif|svg|eot|woff|ttf)\Z/
-
-# Minimum Sass number precision required by bootstrap-sass.
-# See https://github.com/twbs/bootstrap-sass#bower-with-rails
-::Sass::Script::Number.precision = [ 8, ::Sass::Script::Number.precision ].max
